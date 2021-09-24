f2b886eededa8f4ace7378b66b345e63f5bb2ac7
Angel Barros <albarros1973@gmail.com>
2021-09-24 11:26:44 +0100

    Add User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Generate Users
+
+USERS = [
+  { username: 'John', password: '12345678' },
+  { username: 'Peter', password: '12345678' },
+  { username: 'Kayla', password: '12345678' },
+  { username: 'Ulric', password: '12345678' },
+  { username: 'Frederick', password: '12345678' },
+  { username: 'Morton', password: '12345678' },
+  { username: 'Bud', password: '12345678' },
+  { username: 'Jewel', password: '12345678' },
+  { username: 'Bruno', password: '12345678' },
+  { username: 'Nigek', password: '12345678' },
+].freeze
+
+User.create(USERS) if User.count.zero?
