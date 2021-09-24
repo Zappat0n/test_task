6fff6e764b660e1f7bf4b3333898379bebddf1e7
Angel Barros <albarros1973@gmail.com>
2021-09-24 11:48:50 +0100

    Add tests for User model

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rexml', '~> 3.2', '>= 3.2.5'
+  gem 'rspec-rails', '~> 5.0.0'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'Happy path' do
+    expect(User.count).to eq 0
+    user = User.create(username: 'John', password: 'abcdefg')
+    expect(User.count).to eq 1
+  end
+
+  it 'Does not allow empty username' do
+    user = User.create(username: '', password: 'abcdefg')
+    expect(user.valid?).to be false
+  end
+
+  it 'Does not allow blank password' do
+    user = User.create(username: 'John', password: '')
+    expect(user.valid?).to be false
+
+    user = User.create(username: 'John')
+    expect(user.valid?).to be false
+  end
+end
