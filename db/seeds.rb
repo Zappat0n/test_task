15e2f68c48df7910d57db1a9a0e64123faf924be
Angel Barros <albarros1973@gmail.com>
2021-09-24 12:20:09 +0100

    Add seeding for Rating and Job models

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,26 @@ USERS = [
 ].freeze
 
 User.create(USERS) if User.count.zero?
+
+# Generate Ratings
+
+if Rating.count.zero?
+  User.all.each { |user| Rating.create(rating_value: rand(1..5), user: user) }
+end
+
+# Generate Jobs
+
+JOBS = [
+  { title: 'Baker'},
+  { title: 'Butcher'},
+  { title: 'Cook'},
+  { title: 'Farmer'},
+  { title: 'Fireman'},
+  { title: 'Gardener'},
+  { title: 'Hairdresser'},
+  { title: 'Journalist'},
+  { title: 'Lawyer'},
+  { title: 'Mason'}
+].freeze
+
+Job.create(JOBS) if Job.count.zero?
