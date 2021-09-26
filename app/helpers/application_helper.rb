2b75c49e6aecdf0a6246b0395b9cbc3b15a4d5fd
Angel Barros <albarros1973@gmail.com>
2021-09-26 10:34:01 +0100

    Add Navigation Bar layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def logged_in?
+    !session[:user_id].nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if logged_in?
+  end
+
+  def require_login
+    redirect_to login_path, alert: 'You must be logged in to access this section' unless logged_in?
+  end
+
+  def react_component(name, props)
+    content_tag(:div, { id: name, data: { react_props: props }}) do
+    end
+  end
+
 end
